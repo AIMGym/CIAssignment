33bf5d9098a29384dcefd8037ff471605e44ca51
Manuel Gonzales <manuel.gonzales@shaw.ca>
2015-03-07 13:02:03 -0800

    added model and some minor changes to Admin Signed-off-by:Manuel Gonzales <manuel.gonzales@shaw.ca>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -17,8 +17,8 @@ class Admin extends Application {
 
     function index()
     {
-        $this->data['title'] = 'Quotations Maintenance';
-        $this->data['quotes'] = $this->quotes->all();
+        $this->data['title'] = 'Programs Maintenance';
+        $this->data['programs'] = $this->programs->all();
         $this->data['pagebody'] = 'admin_list'; 
         $this->render();
     }
@@ -26,12 +26,12 @@ class Admin extends Application {
     // Add a new quotation
     function add()
     {
-      $quote = $this->quotes->create();
-      $this->present($quote);
+      $program = $this->programs->create();
+      $this->present($program);
     }
     
     // Present a quotation for adding/editing
-    function present($quote)
+    function present($program)
     {
         $message = '';
         if (count($this->errors) > 0)
@@ -41,13 +41,28 @@ class Admin extends Application {
         }
         $this->data['message'] = $message;
         
-        $this->data['fid'] = makeTextField('ID#', 'id', $quote->id,"Unique quote identifier, system-assigned",10,10,true);
-        $this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
-        $this->data['fmug'] = makeTextField('Picture', 'mug', $quote->mug);
-        $this->data['fwhat'] = makeTextArea('The Quote', 'what', $quote->what);
-        $this->data['pagebody'] = 'quote_edit';
+        $this->data['fid'] = makeTextField('ID#', 'id', $program->id,"Unique quote identifier, system-assigned",10,10,true);
+        $this->data['fname'] = makeTextField('Name', 'name', $program->name);
+        $this->data['fcaption'] = makeTextField('Caption', 'caption', $program->caption);
+        $this->data['fdescription'] = makeTextArea('Description', 'description', set_value('description'), 'Detailed description of the program', 500, 40, 5);
+        $this->data['fcontact'] = makeTextField('Contact', 'contact', set_value('contact'), 'Telephone number and/or email to get hold of the attraction');
+        $this->data['flocation'] = makeTextField('Location', 'location', set_value('location'), 'The address of where the attraction is located');
+        $this->data['fprice'] = makeTextField('Price', 'price', set_value('price'), 'Cost of the attraction');
+        $this->data['fimage1'] = makeFileUploader('Main Picture', 'picmain', 'Choose a file to replace this picture');
+        $this->data['fimage2'] = makeFileUploader('Picture 1', 'pic1', 'Choose a file to replace this picture');
+        $this->data['fimage3'] = makeFileUploader('Picture 2', 'pic2', 'Choose a file to replace this picture');
+        $this->data['fimage4'] = makeFileUploader('Picture 3', 'pic3', 'Choose a file to replace this picture');
+        $this->data['fhotel_stars'] = makeComboField('Hotel Stars', 'hotel_stars', '0', $hotel_stars, 'The hotel rating');
+        $this->data['ffood_type'] = makeTextField('Food Type', 'food_type', set_value('food_type'), 'Type of Cuisine');
+        $this->data['fmenu_url'] = makeTextField('Link to Menu', 'menu_url', set_value('menu_url'), 'Link to restaurants\'s menu');        
+        $this->data['fsubmit'] = makeSubmitButton('Add Attraction', 'Add');
         
-        $this->data['fsubmit'] = makeSubmitButton('Process Quote', "Click here to validate the quotation data", 'btn-success');
+        
+        
+        
+        $this->data['pagebody'] = 'program_edit';
+        
+        $this->data['fsubmit'] = makeSubmitButton('Add Program', "Click here to validate the quotation data", 'btn-success');
         
         $this->render();
     }
@@ -56,7 +71,7 @@ class Admin extends Application {
     function confirm()
     {
         
-        $record = $this->quotes->create();
+        $record = $this->programs->create();
         // Extract submitted fields
         $record->id = $this->input->post('id');
         $record->who = $this->input->post('who');
