22b74368dca457cba9d4afd76e64f8a32113f06f
Manuel Gonzales <manuel.gonzales@shaw.ca>
2015-03-07 13:18:52 -0800

    Added an updated css and js library Signed-off-by:Manuel Gonzales <manuel.gonzales@shaw.ca>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -43,19 +43,15 @@ class Admin extends Application {
         
         $this->data['fid'] = makeTextField('ID#', 'id', $program->id,"Unique quote identifier, system-assigned",10,10,true);
         $this->data['fname'] = makeTextField('Name', 'name', $program->name);
-        $this->data['fcaption'] = makeTextField('Caption', 'caption', $program->caption);
-        $this->data['fdescription'] = makeTextArea('Description', 'description', set_value('description'), 'Detailed description of the program', 500, 40, 5);
-        $this->data['fcontact'] = makeTextField('Contact', 'contact', set_value('contact'), 'Telephone number and/or email to get hold of the attraction');
-        $this->data['flocation'] = makeTextField('Location', 'location', set_value('location'), 'The address of where the attraction is located');
-        $this->data['fprice'] = makeTextField('Price', 'price', set_value('price'), 'Cost of the attraction');
+        $this->data['fcaption'] = makeTextField('Caption', 'caption', $program->caption, 'Caption for the main picture');
+        $this->data['fdescription'] = makeTextArea('Description', 'description', $program->description, 'Detailed description of the program', 500, 40, 5);
+        $this->data['fcontact'] = makeTextField('Contact', 'contact', $program->contact,'Telephone number and/or email to join the program');
+        $this->data['flocation'] = makeTextField('Location', 'location', $program->location, 'The address/location of the program');
+        $this->data['fprice'] = makeTextField('Price', 'price', $program->price, 'Cost of the program monthly');
         $this->data['fimage1'] = makeFileUploader('Main Picture', 'picmain', 'Choose a file to replace this picture');
-        $this->data['fimage2'] = makeFileUploader('Picture 1', 'pic1', 'Choose a file to replace this picture');
-        $this->data['fimage3'] = makeFileUploader('Picture 2', 'pic2', 'Choose a file to replace this picture');
-        $this->data['fimage4'] = makeFileUploader('Picture 3', 'pic3', 'Choose a file to replace this picture');
-        $this->data['fhotel_stars'] = makeComboField('Hotel Stars', 'hotel_stars', '0', $hotel_stars, 'The hotel rating');
-        $this->data['ffood_type'] = makeTextField('Food Type', 'food_type', set_value('food_type'), 'Type of Cuisine');
-        $this->data['fmenu_url'] = makeTextField('Link to Menu', 'menu_url', set_value('menu_url'), 'Link to restaurants\'s menu');        
-        $this->data['fsubmit'] = makeSubmitButton('Add Attraction', 'Add');
+        $this->data['fimage2'] = makeFileUploader('Picture 1', 'pic1', 'Choose a file');
+        $this->data['fimage3'] = makeFileUploader('Picture 2', 'pic2', 'Choose a file');
+        $this->data['fimage4'] = makeFileUploader('Picture 3', 'pic3', 'Choose a file');
         
         
         
