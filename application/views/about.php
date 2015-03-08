9c7dffcd3bb300ab7115dc976886b32d5432628d
Adam Jesse <ak1jesse@gmail.com>
2015-03-08 15:07:40 -0700

    Reorganized/reworked some views Signed-off-by:Adam Jesse <ak1jesse@gmail.com>

--- application/views/about.php
+++ application/views/about.php
@@ -4,14 +4,14 @@
         
         <p>AIM Gym opened it’s doors in Downtown Vancouver in 2007. This brand new state-of-the-art club redefined the standard for gyms across the Lower Mainland.  To keep up with demand for more locations, AIM Gym merged with Fitness World in 2009, creating the AIM Fitness World and Sports Club brand. With over 50 years of experience in the fitness industry our dedication to our members is unsurpassed. 
 
-<br>Inline with Steve’s vision of promoting a healthy and balanced lifestyle for all, we have designed our gyms to maximize our members’ experience. With the latest equipment, exclusive group fitness classes, and the best certified personal trainers we are here to help you achieve long term results. 
+<br><br>Inline with Steve’s vision of promoting a healthy and balanced lifestyle for all, we have designed our gyms to maximize our members’ experience. With the latest equipment, exclusive group fitness classes, and the best certified personal trainers we are here to help you achieve long term results. 
 
-<br>Our world class team will help you understand the fundamentals of an effective exercise program and will keep you informed on the latest in health and fitness. At AIM Fitness World and Sports Club we believe in providing the highest quality service in a fun and friendly environment. </p>
+<br><br>Our world class team will help you understand the fundamentals of an effective exercise program and will keep you informed on the latest in health and fitness. At AIM Fitness World and Sports Club we believe in providing the highest quality service in a fun and friendly environment. <br></p>
         
     
     <!-- end main content-->
     
-    
+            
          <img src="{image}"/>
         <h2>{name}</h2>
         <p>{description}<br/>
--- data/programs.sql
+++ data/programs.sql
@@ -47,13 +47,13 @@ CREATE TABLE IF NOT EXISTS `programs` (
 
 
 INSERT INTO `programs`(`name`, `caption`, `description`, `location`, `price`, `image1`, `image2`, `image3`, `image4`)
-VALUES ('Personal Traning', 'Personal training Services', 'Fitness testing includes strength analysis, postural analysis, gait analysis, movement imbalances, body composition, and blood pressure.
+VALUES ('Personal Traning', 'Personal services tailored for your body', 'Fitness testing includes strength analysis, postural analysis, gait analysis, movement imbalances, body composition, and blood pressure.
 Consultation includes discussion of scores, norms, goal setting, educational material and an individualized exercise program.
 All individuals who are involved in the group and individual training program must have a PAR-Q, Assumption of Risk form, and a Cardiovascular Health Screening Questionnaire on file before they are allowed to participate. These can be completed at the first meeting with the personal trainer.
 Individual features of each component will depend upon the needs of the each client.', 'SURREY', '40.0', '/data/images/personaltraining1.jpg', '/data/images/personaltraining2.jpg', '/data/images/personaltraining3.jpg', '/data/images/personaltraining4.jpg');
 
 INSERT INTO `programs`(`name`, `caption`, `description`, `location`, `price`, `image1`, `image2`, `image3`, `image4`)
-VALUES ('Yoga', 'Yoga Stuff', '<strong>Ashtanga Yoga:</strong>
+VALUES ('Yoga', 'Yoga classes catering to your skill level', '<strong>Ashtanga Yoga:</strong>
 A type of yoga class that helps you to work on deeper stretching and modifications within each pose. It is a series of the core poses of yoga. This class will focus on correct alignment and pose positioning.
 <strong>Lunch Time Yoga:</strong>
 A forty minute quick and easy reset to a stressful day.
@@ -66,5 +66,5 @@ Slow Flow Yoga is aimed at helping participants get in touch with their bodies a
 breath work, visualization, and meditation.', 'VANCOUVER / SURREY', '49.99', '/data/images/yoga1.jpg', '/data/images/yoga2.jpg', '/data/images/yoga3.jpg', '/data/images/yoga4.jpg');
 
 INSERT INTO `programs`(`name`, `caption`, `description`, `location`, `price`, `image1`, `image2`, `image3`, `image4`)
-VALUES ('AIM Gym Kung-fu', 'Our one and only martial art program', 'This format combines various forms of martial arts and self defense moves with aerobics producing a great workout for the whole body.  Using a non-contact approach, learn how to throw a right cross, kick with purpose, or bob and weave in the ring. 
+VALUES ('AIM Gym Kung-fu', 'The always exciting martial arts program', 'This format combines various forms of martial arts and self defense moves with aerobics producing a great workout for the whole body.  Using a non-contact approach, learn how to throw a right cross, kick with purpose, or bob and weave in the ring. 
  Hand wraps not required but may help the individual focus on hit spots and accuracy.', 'BURNABY / VANCOUVER', '29.99', '/data/images/martialarts1.jpg', '/data/images/martialarts2.jpg', '/data/images/martialarts3.jpg', '/data/images/martialarts4.jpg');
