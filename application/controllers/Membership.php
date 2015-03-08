175e791df8e8eb934227e3449ea607e1cb6bd6a5
Adam Jesse <ak1jesse@gmail.com>
2015-03-07 19:05:42 -0800

    Updated membership controller Signed-off-by:Adam Jesse <ak1jesse@gmail.com>

--- application/controllers/Membership.php
+++ application/controllers/Membership.php
@@ -15,14 +15,39 @@ class Membership extends Application{
     
     function index() {
         
+        //Required page
         $this->data['pagebody'] = 'Membership';
         
-        $gym_info = $this->Gymdata->get(3);
-       
-        $this->data['name'] = $gym_info['name'];
-        $this->data['image'] = $gym_info['image'];
-        $this->data['description'] = $gym_info['description'];
+        //Retrieve all programs from relevant database
+        $membership = $this->Membership->getall();
+        $this->data['Membership'] = $membership;
         
         $this->render();
     }
+    
+    //Returns a specified page regarding memberships
+    function getOne($_id) {
+
+        //Required page
+        $this->data['pagebody'] = 'Membership_sub';
+
+        //Retrieve info for specified page by id
+        $record = $this->Memberships->get($_id);
+
+        //Populating page data
+        $this->data['id'] = $record->id;
+        $this->data['name'] = $record->name;
+        $this->data['caption'] = $record->caption;
+        $this->data['description'] = $record->description;
+        $this->data['price'] = $record->price;
+        $this->data['image1'] = $record->image1;
+        $this->data['image2'] = $record->image2;
+        $this->data['image3'] = $record->image3;
+        $this->data['image4'] = $record->image4;
+
+        $this->render();
+    }
+
 }
+/* End of file Memership.php */
+/* Location: application/controllers/Memership.php */
