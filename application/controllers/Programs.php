eca192e63b4793469c0842f7366f5472205e17db
Adam Jesse <ak1jesse@gmail.com>
2015-03-07 19:06:04 -0800

    Updated programs controller Signed-off-by:Adam Jesse <ak1jesse@gmail.com>

--- application/controllers/Programs.php
+++ application/controllers/Programs.php
@@ -17,12 +17,38 @@ class Programs extends Application{
         
         $this->data['pagebody'] = 'Programs';
         
-        $gym_info = $this->Gymdata->get(4);
-       
-        $this->data['name'] = $gym_info['name'];
-        $this->data['image'] = $gym_info['image'];
-        $this->data['description'] = $gym_info['description'];
+        //Retrieve all programs from relevant database
+        $programs = $this->Programs->getall();
+        $this->data['Programs'] = $programs;
         
         $this->render();
     }
+    
+    //Returns a specified page regarding a program
+    function getOne($_id) {
+
+        //Required page
+        $this->data['pagebody'] = 'Programs_sub';
+
+        //Retrieve info for specified page by id
+        $record = $this->Programs->get($_id);
+
+        //Populating page data
+        $this->data['id'] = $record->id;
+        $this->data['name'] = $record->name;
+        $this->data['caption'] = $record->caption;
+        $this->data['description'] = $record->description;
+        $this->data['data_added'] = $record->date_added;
+        $this->data['location'] = $record->location;
+        $this->data['price'] = $record->price;
+        $this->data['image1'] = $record->image1;
+        $this->data['image2'] = $record->image2;
+        $this->data['image3'] = $record->image3;
+        $this->data['image4'] = $record->image4;
+
+        $this->render();
+    }
+
 }
+/* End of file Programs.php */
+/* Location: application/controllers/Programs.php */
